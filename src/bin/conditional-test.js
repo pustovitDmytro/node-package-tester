16d92519fbc5d8107b47f6cd7df7656cb6ec59a6
Dmytro Pustovit <dipustovit@gmail.com>
2022-01-28 18:47:32 +0200

    Fix: drop fs-extra from conditional-test.js

--- src/bin/conditional-test.js
+++ src/bin/conditional-test.js
@@ -1,6 +1,5 @@
 import { spawn, execSync } from 'child_process';
 import path from 'path';
-import fs from 'fs-extra';
 import {
     getTestCommand,
     getPrepareCommands
@@ -10,7 +9,8 @@ async function main() {
     const { platform, versions } = process;
 
     console.log(JSON.stringify({ platform, versions }));
-    const packageJSON = await fs.readJSON(path.join(process.cwd(), 'package.json'));
+    // eslint-disable-next-line security/detect-non-literal-require
+    const packageJSON = require(path.join(process.cwd(), 'package.json'));
     const config = packageJSON['node-package-tester'];
     const testCommand = getTestCommand(config, { platform, versions });
     const prepareCommands = getPrepareCommands(config, { platform, versions });
